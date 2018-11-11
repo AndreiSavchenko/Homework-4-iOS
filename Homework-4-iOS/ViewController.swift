100da300368756382b0a64f363fe25d7c0038ca6
AndreiSavchenko <andrei.savchenko.ad@gmail.com>
2018-11-11 16:54:06 +0200

    Initial Commit

---
+++ Homework-4-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework-4-iOS
+//
+//  Created by Alla on 11/11/18.
+//  Copyright © 2018 AndreiSavchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
